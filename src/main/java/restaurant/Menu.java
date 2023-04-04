50d71207560ea64e29cf8fb5f434de87a4436228
kelseyjungermann <kelsey.jungemrann@gmail.com>
2023-04-03 19:28:58 -0500

    initial commit

---
+++ src/main/java/restaurant/Menu.java
@@ -0,0 +1,7 @@
+package restaurant;
+import java.util.ArrayList;
+import java.util.Date;
+public class Menu {
+ private ArrayList<MenuItem> menu = new ArrayList<>();
+ private Date lastUpdated = new Date();
+}
