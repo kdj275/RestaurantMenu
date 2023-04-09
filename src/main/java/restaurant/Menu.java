b2a89c0b91fcbfb9063d41fb4ceb5457d5b8e16f
kelseyjungermann <kelsey.jungemrann@gmail.com>
2023-04-09 16:55:19 -0500

    initial commit

--- src/main/java/restaurant/Menu.java
+++ src/main/java/restaurant/Menu.java
@@ -4,4 +4,34 @@ import java.util.Date;
 public class Menu {
  private ArrayList<MenuItem> menu = new ArrayList<>();
  private Date lastUpdated = new Date();
+// A way to add and remove menu items from the menu.
+ public void addItem(MenuItem newItem) {
+  menu.add(newItem);
+  lastUpdated = new Date();
+ }
+ public void removeItem(MenuItem oldItem) {
+  menu.remove(oldItem);
+  lastUpdated = new Date();
+ }
+
+
+
+// A way to tell when the menu was last updated.
+ public Date getLastUpdated() {
+  return lastUpdated;
+ }
+
+ // A way to print out both a single menu item as well as the entire menu.
+ public void printMenu() {
+  for(MenuItem item : menu) {
+   System.out.println(item.getName());
+   System.out.println(item.getDescription());
+
+  }
+ }
+ public void printMenuItem(MenuItem item) {
+  System.out.println();
+ }
+
+
 }
--- src/main/java/restaurant/MenuItem.java
+++ src/main/java/restaurant/MenuItem.java
@@ -1,11 +1,14 @@
 package restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
     private String name;
     private String description;
     private String category;
     private double price;
     private boolean isNew;
+    private Date dateAdded;
 
     public MenuItem(String name, String description, String category, double price, boolean isNew) {
         this.name = name;
@@ -13,6 +16,7 @@ public class MenuItem {
         this.category = category;
         this.price = price;
         this.isNew = isNew;
+        this.dateAdded = new Date();
     }
 
     public String getName() {
@@ -54,4 +58,22 @@ public class MenuItem {
     public void setNew(boolean aNew) {
         isNew = aNew;
     }
+
+    // A way to determine whether or not two menu items are equal.
+    public boolean isEqual(MenuItem item) {
+    }
+
+    // A way to tell if a menu item is new.
+    public void isNewItem() {
+       Date compareDate = new Date(2023,1,1);
+        boolean isItNew = this.dateAdded.after(compareDate);
+        if (isItNew) {
+            this.isNew = true;
+            System.out.println("This item is new.");
+        } else {
+            this.isNew = false;
+            System.out.println("This item is not new.");
+        }
+    }
 }
+
